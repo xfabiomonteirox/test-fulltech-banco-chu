92dadaa6fb049f5155b457ffddb65aa6cde11611
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-03 21:50:17 -0300

    Create base entity

---
+++ BancoChu/BancoChu.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace BancoChu.Domain.Entities.Base;
+
+public abstract class BaseEntity
+{
+    protected BaseEntity(Guid id) => Id = id;
+
+    protected BaseEntity()
+    { }
+
+    public Guid Id { get; private init; }
+}
