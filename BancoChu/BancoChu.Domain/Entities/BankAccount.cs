b46d8010103ad89c7c4f1d1c8dcc6ad125aff9f0
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-03 21:50:38 -0300

    Create bannk account entity

---
+++ BancoChu/BancoChu.Domain/Entities/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using BancoChu.Domain.Entities.Base;
+
+namespace BancoChu.Domain.Entities;
+
+public sealed class BankAccount : BaseEntity
+{
+    internal BankAccount(Guid id,
+        int branchCode,
+        int currentAccountNumber,
+        int accountBalance) : base(id)
+    {
+        BranchCode = branchCode;
+        CurrentAccountNumber = currentAccountNumber;
+        AccountBalance = accountBalance;
+    }
+
+    private BankAccount()
+    {
+    }
+
+    public Guid BankAccountId { get; private set; }
+    public int BranchCode { get; set; }
+    public int CurrentAccountNumber { get; set; }
+    public int AccountBalance { get; set; }
+}
