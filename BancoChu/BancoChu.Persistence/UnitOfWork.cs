1f95ced3c3b8e34a034eaa770857543e06adf468
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-04 20:29:09 -0300

    Create UnitOfWork

---
+++ BancoChu/BancoChu.Persistence/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using BancoChu.Domain.Repositories;
+
+namespace BancoChu.Persistence;
+
+internal class UnitOfWork(ApplicationDbContext dbContext) : IUnitOfWork
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        => _dbContext.SaveChangesAsync(cancellationToken);
+}
