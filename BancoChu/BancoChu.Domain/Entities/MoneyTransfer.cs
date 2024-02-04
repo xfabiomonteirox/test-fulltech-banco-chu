6d33714e93731d566a43bcc811b13c3c7f164661
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-04 20:18:46 -0300

    Create a MoneyTransfer entity

---
+++ BancoChu/BancoChu.Domain/Entities/MoneyTransfer.cs
@@ -0,0 +1,32 @@
+﻿using BancoChu.Domain.Entities.Base;
+
+namespace BancoChu.Domain.Entities
+{
+    public sealed class MoneyTransfer : BaseEntity
+    {
+        internal MoneyTransfer(Guid id,
+            decimal amount,
+            Guid accountId,
+            string description,
+            string destination) : base(id)
+        {
+            Amount = amount;
+            MadeOn = DateTime.Now;
+            AccountId = accountId;
+            Description = description;
+            Destination = destination;
+            Destination = destination;
+        }
+
+        private MoneyTransfer()
+        {
+        }
+
+        public decimal Amount { get; set; }
+        public DateTime MadeOn { get; set; }
+        public Guid AccountId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Destination { get; set; } = string.Empty;
+        public BankAccount BankAccount { get; set; } = default!;
+    }
+}
