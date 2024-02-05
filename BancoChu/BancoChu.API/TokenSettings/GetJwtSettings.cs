e61dd7074abd52595ec64af70cb157d4a437ae4d
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-05 20:43:48 -0300

    Add jwt settings

---
+++ BancoChu/BancoChu.API/TokenSettings/GetJwtSettings.cs
@@ -0,0 +1,12 @@
+﻿using BancoChu.Infrastructure.Authentication;
+
+namespace BancoChu.API.TokenSettings;
+
+public class GetJwtSettings(IConfiguration configuration)
+{
+    private const string SectionName = "Jwt";
+    private readonly IConfiguration _configuration = configuration;
+
+    public void Configure(JwtOptions options) =>
+        _configuration.GetSection(SectionName).Bind(options);
+}
---
+++ BancoChu/BancoChu.API/TokenSettings/JwtOptionsSettings.cs
@@ -0,0 +1,21 @@
+﻿using BancoChu.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BancoChu.API.TokenSettings
+{
+    public class JwtOptionsSettings(JwtOptions jwtOptions) : IPostConfigureOptions<JwtBearerOptions>
+    {
+        private readonly JwtOptions _jwtOptions = jwtOptions;
+
+        public void PostConfigure(string? name, JwtBearerOptions options)
+        {
+            options.TokenValidationParameters.ValidIssuer = _jwtOptions.Issuer;
+            options.TokenValidationParameters.ValidAudience = _jwtOptions.Audience;
+            options.TokenValidationParameters.IssuerSigningKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey));
+        }
+    }
+}
