56f5b36e972dd5562a239bd99eea41a32f852aec
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-06 00:06:00 -0300

    Add ApplicationUser entity

---
+++ BancoChu/BancoChu.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using BancoChu.Domain.Entities.Base;
+
+namespace BancoChu.Domain.Entities;
+
+public sealed class ApplicationUser : BaseEntity
+{
+    internal ApplicationUser(Guid id,
+        string email) : base(id)
+    {
+        Email = email;
+    }
+
+    private ApplicationUser()
+    {
+    }
+
+    public string Email { get; private set; } = null!;
+}
---
+++ BancoChu/BancoChu.Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using BancoChu.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BancoChu.Persistence.Configurations;
+
+internal sealed class ApplicationUserConfiguration
+    : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable(nameof(ApplicationUser));
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Email).IsUnique();
+    }
+}
