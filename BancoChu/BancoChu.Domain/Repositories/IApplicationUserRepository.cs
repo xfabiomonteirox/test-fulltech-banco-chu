737bb82678cb0bb8cc7369afbea6d72238ca473a
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-07 17:24:49 -0300

    Create ApplicationUserRepository

---
+++ BancoChu/BancoChu.Domain/Repositories/IApplicationUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using BancoChu.Domain.Entities;
+
+namespace BancoChu.Domain.Repositories;
+
+public interface IApplicationUserRepository
+{
+    Task<ApplicationUser?> GetByEmailAsync(
+        string email,
+        CancellationToken cancellationToken = default);
+}
