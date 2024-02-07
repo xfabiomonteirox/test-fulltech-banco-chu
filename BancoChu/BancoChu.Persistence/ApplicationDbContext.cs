63fc4ff36e29a6c63c7bacf450995a24202c8348
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-07 17:23:07 -0300

    Add DbSet

--- BancoChu/BancoChu.Persistence/ApplicationDbContext.cs
+++ BancoChu/BancoChu.Persistence/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BancoChu.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BancoChu.Persistence;
 
 public sealed class ApplicationDbContext(DbContextOptions options) : DbContext(options)
 {
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<BankAccount> BankAccounts { get; set; }
+    public DbSet<MoneyTransfer> MoneyTransfers { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder) =>
         modelBuilder.ApplyConfigurationsFromAssembly(AssemblyReference.Assembly);
 }
