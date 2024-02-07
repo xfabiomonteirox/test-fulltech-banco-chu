b4fcd9afe5947e8bd66e2b63327b2460ccc05736
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-07 17:25:57 -0300

    Create MapEndpoints

---
+++ BancoChu/BancoChu.API/MapBankChuEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using BancoChu.Application.Authentication;
+using BancoChu.Domain.Shared;
+using MediatR;
+
+namespace BancoChu.API;
+
+public static class BankChuMapEndpoints
+{
+    public static void MapBankChuEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("v1/login", async (
+            IMediator mediator,
+            string email) =>
+        {
+            Result<string> token = await mediator.Send(new AuthenticationCommand(email));
+            return token.IsSuccess
+                ? Results.Created(string.Empty, token.Value)
+                : Results.BadRequest(token.Error);
+        });
+    }
+}
