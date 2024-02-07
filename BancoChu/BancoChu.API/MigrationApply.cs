c7fbaff331f8346a76e5357d3031c3ab60b9ce52
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-07 17:21:00 -0300

    Create migration extension class

---
+++ BancoChu/BancoChu.API/MigrationApply.cs
@@ -0,0 +1,17 @@
+﻿using BancoChu.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BancoChu.API;
+
+public static class MigrationApply
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        using ApplicationDbContext dbContext =
+            scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+}
