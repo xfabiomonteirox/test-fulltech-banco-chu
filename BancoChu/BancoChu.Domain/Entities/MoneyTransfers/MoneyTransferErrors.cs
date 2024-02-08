508fb2f0f59a2d5c0e425d34109ad82b24873459
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-08 12:33:08 -0300

    Create a new error to brazil holiday validation

--- BancoChu/BancoChu.Domain/Entities/MoneyTransfers/MoneyTransferErrors.cs
+++ BancoChu/BancoChu.Domain/Entities/MoneyTransfers/MoneyTransferErrors.cs
@@ -7,4 +7,8 @@ public static class MoneyTransferErrors
     public static Error NotFoundBranchCode(int branchCode) => Error.NotFound(
         "MoneyTransfer.NotFoundBranchCode",
         $"The Account with the Code = '{branchCode}' was not found");
+
+    public static Error NotAllowedBrazilHoliday() => Error.Failure(
+    "MoneyTransfer.NotAllowed",
+    "Transfers are not allowed on national holidays, so try again with a different date!");
 }
