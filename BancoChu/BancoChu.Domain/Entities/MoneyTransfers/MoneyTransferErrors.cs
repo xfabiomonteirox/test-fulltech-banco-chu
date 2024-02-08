624adf307a77625c19586470eeb037ea8f01fc58
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-08 00:29:27 -0300

    Add MoneyTransferErrors

---
+++ BancoChu/BancoChu.Domain/Entities/MoneyTransfers/MoneyTransferErrors.cs
@@ -0,0 +1,10 @@
+﻿using BancoChu.Domain.Shared;
+
+namespace BancoChu.Domain.Entities.MoneyTransfers;
+
+public static class MoneyTransferErrors
+{
+    public static Error NotFoundBranchCode(int branchCode) => Error.NotFound(
+        "MoneyTransfer.NotFoundBranchCode",
+        $"The Account with the Code = '{branchCode}' was not found");
+}
