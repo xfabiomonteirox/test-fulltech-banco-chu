19d2fbecda06bdfd12b08cc69397aaee952e57e1
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-08 00:29:09 -0300

    Add GetByBranchCodeAndAccountNumberAsync

--- BancoChu/BancoChu.Domain/Repositories/IBankAccountRepository.cs
+++ BancoChu/BancoChu.Domain/Repositories/IBankAccountRepository.cs
@@ -5,4 +5,9 @@ namespace BancoChu.Domain.Repositories;
 public interface IBankAccountRepository
 {
     void Add(BankAccount bankAccount);
+
+    Task<BankAccount?> GetByBranchCodeAndAccountNumberAsync(
+        int branchCode,
+        int accountNumber,
+        CancellationToken cancellationToken = default);
 }
--- BancoChu/BancoChu.Persistence/Repositories/BankAccountRepository.cs
+++ BancoChu/BancoChu.Persistence/Repositories/BankAccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BancoChu.Domain.Entities.BankAccounts;
 using BancoChu.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace BancoChu.Persistence.Repositories;
 
@@ -9,4 +10,11 @@ public sealed class BankAccountRepository(ApplicationDbContext dbContext) : IBan
 
     public void Add(BankAccount bankAccount)
         => _dbContext.Set<BankAccount>().Add(bankAccount);
+
+    public async Task<BankAccount?> GetByBranchCodeAndAccountNumberAsync(
+        int branchCode,
+        int accountNumber,
+        CancellationToken cancellationToken = default)
+        => await _dbContext.BankAccounts.FirstOrDefaultAsync(x =>
+            x.BranchCode == branchCode && x.CurrentAccountNumber == accountNumber, cancellationToken);
 }
