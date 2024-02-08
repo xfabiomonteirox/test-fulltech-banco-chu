3c5f2b27a408692fca586a73e2da4700f17b9868
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-08 00:28:05 -0300

    Create MoneyTransferRepository

---
+++ BancoChu/BancoChu.Domain/Repositories/IMoneyTransferRepository.cs
@@ -0,0 +1,8 @@
+﻿using BancoChu.Domain.Entities.MoneyTransfers;
+
+namespace BancoChu.Domain.Repositories;
+
+public interface IMoneyTransferRepository
+{
+    void Add(MoneyTransfer moneyTransfer);
+}
---
+++ BancoChu/BancoChu.Persistence/Repositories/MoneyTransferRepository.cs
@@ -0,0 +1,12 @@
+﻿using BancoChu.Domain.Entities.MoneyTransfers;
+using BancoChu.Domain.Repositories;
+
+namespace BancoChu.Persistence.Repositories;
+
+public sealed class MoneyTransferRepository(ApplicationDbContext dbContext) : IMoneyTransferRepository
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    public void Add(MoneyTransfer moneyTransfer)
+        => _dbContext.Set<MoneyTransfer>().Add(moneyTransfer);
+}
