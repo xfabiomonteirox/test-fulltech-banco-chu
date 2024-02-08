31001d747f312fada36e35498d6c06bc6554c6d0
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-08 00:28:31 -0300

    Create MoneyTransferCommand

---
+++ BancoChu/BancoChu.Application/MoneyTransfer/Commands/CreateMoneyTransferCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using BancoChu.Application.Abstractions;
+using BancoChu.Domain.Entities.MoneyTransfers;
+using BancoChu.Domain.Repositories;
+using BancoChu.Domain.Shared;
+
+namespace BancoChu.Application.MoneyTransfer.Commands;
+
+internal sealed class CreateMoneyTransferCommandHandler(
+    IUnitOfWork unitOfWork,
+    IMoneyTransferRepository moneyTransferRepository,
+    IBankAccountRepository bankAccountRepository
+    )
+    : ICommandHandler<CreateMoneyTransferCommand, Guid>
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMoneyTransferRepository _moneyTransferRepository = moneyTransferRepository;
+    private readonly IBankAccountRepository _bankAccountRepository = bankAccountRepository;
+
+    public async Task<Result<Guid>> Handle(CreateMoneyTransferCommand request, CancellationToken cancellationToken)
+    {
+        var bankAccount = await _bankAccountRepository
+            .GetByBranchCodeAndAccountNumberAsync(request.BranchCode, request.AccountNumber, cancellationToken);
+
+        if (bankAccount is null)
+            return Result.Failure<Guid>(MoneyTransferErrors.NotFoundBranchCode(request.BranchCode));
+
+        bankAccount.AccountBalance -= request.Amount;
+
+        var moneyTransfer = new Domain.Entities.MoneyTransfers.MoneyTransfer(
+            Guid.NewGuid(),
+            bankAccount,
+            request.Amount,
+            request.MadeOn,
+            request.Description,
+            request.Destination);
+
+        _moneyTransferRepository.Add(moneyTransfer);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return moneyTransfer.Id;
+    }
+}
