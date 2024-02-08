d9af4c65e5d987b89f91f086961b2222edf971ed
Fabio <25624589+xfabiomonteirox@users.noreply.github.com>
2024-02-07 22:42:48 -0300

    Create CreateBankAccountCommand

---
+++ BancoChu/BancoChu.Application/BankAccount/Commands/CreateBankAccountCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using BancoChu.Application.Abstractions;
+using BancoChu.Domain.Repositories;
+using BancoChu.Domain.Shared;
+
+namespace BancoChu.Application.BankAccount.Commands;
+
+internal sealed class CreateBankAccountCommandHandler(
+    IUnitOfWork unitOfWork,
+    IBankAccountRepository bankAccountRepository)
+    : ICommandHandler<CreateBankAccountCommand, Guid>
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IBankAccountRepository _bankAccountRepository = bankAccountRepository;
+
+    public async Task<Result<Guid>> Handle(CreateBankAccountCommand request, CancellationToken cancellationToken)
+    {
+        var bankAccount = new Domain.Entities.BankAccounts.BankAccount(
+            Guid.NewGuid(),
+            request.BranchCode,
+            request.CurrentAccountNumber,
+            request.AccountBalance);
+
+        _bankAccountRepository.Add(bankAccount);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return bankAccount.Id;
+    }
+}
